26cc8fa2ab99b18d13a119f953e9bf13ccc67b5d
Antibiaka <47697073+Antibiaka@users.noreply.github.com>
2020-12-05 22:52:20 +0100

    arrows & walls
    stuck , destroy , rotate

---
+++ PracaDomowa10_s15093/Assets/ArrowCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Wall")) {
+
+            rb.velocity = Vector2.zero;
+            rb.isKinematic = true;
+            rb.GetComponent<BoxCollider2D>().enabled = false;
+            rb.freezeRotation = true;
+        }
+    }
+}
--- PracaDomowa10_s15093/Assets/PlayerMovement.cs
+++ PracaDomowa10_s15093/Assets/PlayerMovement.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed = 3f;
+    public  Rigidbody2D rb;
+    Vector2 move;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Update() {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + move * moveSpeed * Time.deltaTime);
     }
 }
